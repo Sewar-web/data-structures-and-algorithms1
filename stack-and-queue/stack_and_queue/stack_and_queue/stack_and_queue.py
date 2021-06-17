f84ac27b34a02e00dc614c8b0595bb784f2bacf7
Sewar-web <s.maqableh@yahoo.com>
2021-06-17 16:22:54 +0300

    cc10

---
+++ stack-and-queue/stack_and_queue/stack_and_queue/stack_and_queue.py
@@ -0,0 +1,75 @@
+class Node:
+    def __init__(self ,value) :
+         self.value=value
+         self.next=None
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+    def push(self, value):
+        node = Node(value)
+        node.next = self.top
+        self.top = node
+    def pop(self):
+       try:
+           temp=self.top
+           self.top = self.top.next
+           self.top.next = None
+           return temp.value
+       except:
+           return 'Stack is Empty'
+
+           
+    def peek(self):
+
+        return self.top.value
+
+    def is_empty(self): 
+        return self.top == None
+        # OR
+        # return True if self.top is None else False
+        # OR
+        # if self.top is None:
+        #     return True
+        # return False
+
+
+
+
+# If we use a list(array), easily do this:
+# data = [3, -7, 'd']
+# data.pop()
+# data.append(5)
+# data[-1]
+# return if len(data) == 0
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+    def enqueue(self, value):
+        node = Node(value)
+        if not self.rear:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.next = node
+            self.rear = node
+
+    def dequeue(self):
+        if self.front:
+            temp = self.front
+            temp2 = temp.next
+            self.front = temp2
+            return temp.value
+        else:
+            return('Queue is Empty')
+
+    def peek(self):
+        return self.front.value
+
+    def is_empty(self):
+        return self.front==None
+
+
--- stack-and-queue/stack_and_queue/tests/test_stack_and_queue.py
+++ stack-and-queue/stack_and_queue/tests/test_stack_and_queue.py
@@ -1,5 +1,91 @@
+from stack_and_queue.stack_and_queue import Node,Stack,Queue
 from stack_and_queue import __version__
-
+import pytest
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+
+def test_push():
+    stack=Stack()
+    stack.push(3)
+    stack.push(-7)
+    stack.push('d')
+    actual = stack.top.value
+    expected = 'd'
+    assert actual == expected
+
+
+def test_pop():
+    stack = Stack()
+    stack.push(3)
+    stack.push(-7)
+    stack.push('d')
+    stack.pop()
+    actual = stack.top.value
+    expected = -7
+    assert actual == expected
+ 
+def test_peek():
+    stack = Stack()
+    stack.push(3)
+    stack.push(-7)
+    stack.push('d')
+    actual = stack.top.value
+    expected = 'd'
+    assert actual == expected
+
+def test_is_empty():
+    stack = Stack()
+    stack.is_empty()
+    actual = stack.top
+    expected = None
+    assert actual == expected
+
+
+def test_enqueue(queue_vals):
+    assert queue_vals.rear.value == 6
+    assert queue_vals.front.value == 8
+
+def test_dequeue():
+    queue=Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.dequeue()
+    actual = queue.front.value
+    expected = 2
+    assert actual == expected
+
+def test_peek():
+    queue=Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    actual = queue.front.value
+    expected = 1
+    assert actual == expected
+
+def test_is_empty():
+    queue=Queue()
+    queue.is_empty()
+    actual = queue.front
+    expected = None
+    assert actual == expected
+
+""" Notes
+6 <- -4 <- 'hi' <- 8
+                   F
+R
+"""
+
+# Decorator
+@pytest.fixture
+def queue_vals():
+    queue = Queue()
+    queue.enqueue(8)
+    queue.enqueue('hi')
+    queue.enqueue(-4)
+    queue.enqueue(6)
+    return queue
