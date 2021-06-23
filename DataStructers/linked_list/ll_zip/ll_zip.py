812d76184833df739c3162555294141f5ff0e791
Sewar-web <s.maqableh@yahoo.com>
2021-06-23 21:30:22 +0300

    done

--- DataStructers/linked_list/ll_zip/ll_zip.py
+++ DataStructers/linked_list/ll_zip/ll_zip.py
@@ -1,27 +1,43 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import Node, LinkedList
 
-def ll_zip(ll1, ll2):
-    current = ll1.head
-    current1 = ll2.head
-    ll = LinkedList()
-    while True:
-        if current != None: 
-            ll.append(current.value)
-            current = current.next 
-        if current1 != None:
-            ll.append(current1.value)
+
+def zipLists(ll1, ll2):
+    current1 = ll1.head
+    current2 = ll2.head
+
+    if current1 == None or current2 == None:
+        if current1:
+            return ll1.__str__()
+        elif current2:
+            return ll2.__str__()
+        else:
+         return "Both of the linked list is empty"
+
+    valuelist = []
+    while current1 or current2:
+        if(current1):
+            valuelist+=[current1.value]
             current1 = current1.next
+        if(current2):
+            valuelist+=[current2.value]
+            current2 = current2.next
+    x=''
+    for i in valuelist:
+      x+=f'( {i} ) -> '
+    x+='None'
+    return x
 
-        if current == None and current1 == None:
-            break
 
-    return ll.head.value
 
 if __name__ == "__main__":
-    ll1=LinkedList()
+    ll1 = LinkedList()
     ll1.append(1)
-    ll1.append(2)
-    ll2=LinkedList()
-    ll2.append(3)
+    ll1.append(3)
+    ll1.append(5)
+    ll2 = LinkedList()
+    ll2.append(2)
     ll2.append(4)
-    print(ll_zip(ll1,ll2))
+    ll2.append(6)
+    print(ll1.__str__())
+    print(ll2.__str__())
+    print(zipLists(ll1, ll2))
