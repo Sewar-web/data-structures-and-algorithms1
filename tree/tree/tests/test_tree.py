6d84aa1f4625b005faf535b4e212be79199bc78d
Sewar-web <s.maqableh@yahoo.com>
2021-06-26 16:45:56 +0300

    done

--- tree/tree/tests/test_tree.py
+++ tree/tree/tests/test_tree.py
@@ -46,16 +46,35 @@ def test_post_order_empty_tree():
 
 
 
+
 def test_BST():
     tree = Binary_Search_Tree()
-    tree.add(5)
-    tree.add(10)
-    tree.add(1)
+    tree.Add(5)
+    tree.Add(10)
+    tree.Add(1)
 
-    tree.add(7)
-    tree.add(4)
-    tree.add(12)
-    tree.add(0)
+    tree.Add(7)
+    tree.Add(4)
+    tree.Add(12)
+    tree.Add(0)
+    assert tree.pre_order()=='510410712'
+    assert tree.in_order()=='014571012'
+    assert tree.post_order()=='041712105'
+
+
+#////////////////////////////////////BTS_Add/////////////////////////////////////////////
+
+
+def test_BST_Add():
+    tree = Binary_Search_Tree()
+    tree.Add(5)
+    tree.Add(10)
+    tree.Add(1)
+
+    tree.Add(7)
+    tree.Add(4)
+    tree.Add(12)
+    tree.Add(0)
     assert tree.root.value == 5
     assert tree.root.left.value == 1
     assert tree.root.right.value == 10
@@ -64,6 +83,34 @@ def test_BST():
     assert tree.root.right.left.value == 7
     assert tree.root.right.right.value == 12
 
+
+#////////////////////////////////////BTS_Contains/////////////////////////////////////////////
+
+
+def test_BST_Contains():
+    tree = Binary_Search_Tree()
+    tree = Binary_Search_Tree()
+    tree.Add(5)
+    tree.Add(10)
+    tree.Add(1)
+    tree.Add(7)
+    tree.Add(4)
+    tree.Add(12)
+    tree.Add(0)
+    assert tree.Contains(5) == True
+    assert tree.Contains(10) == True
+    assert tree.Contains(0) == True
+    assert tree.Contains(12) == True
+
+
+#////////////////////////////////////BTS_is_empty/////////////////////////////////////////////
+
+
+def test_tree_empty():
+    tree = Tree()
+    assert tree.pre_order() == ''
+    assert tree.in_order() == ''
+    assert tree.post_order() == ''
 
 
 
--- tree/tree/tree/tree.py
+++ tree/tree/tree/tree.py
@@ -9,93 +9,156 @@ class Tree:
         self.root = None
 
     def pre_order(self):
-       
-        output = ''
-        if not self.root:
+        try: 
+            output = ''
+            if not self.root:
+                return output
+
+
+            def _traverse(node):
+                nonlocal output # Because output is not accessible
+                output = output + str(node.value) # Root
+
+                if node.left:
+                    _traverse(node.left)
+        
+                if node.right:
+                    _traverse(node.right)
+                return output
+            _traverse(self.root)
+
             return output
-
-
-        def _traverse(node):
-            nonlocal output # Because output is not accessible
-            output = output + str(node.value) # Root
-
-            if node.left:
-                _traverse(node.left)
-      
-            if node.right:
-                _traverse(node.right)
-            return output
-        _traverse(self.root)
-
-        return output
+        except:
+            return "you have error with insertion by pre_order"
 
 
     def in_order(self):
-        output = ''
-        if not self.root:
-            return output
+        try:
+            output = ''
+            if not self.root:
+                return output
 
-        def _traverse(node):
-            nonlocal output
-            if node.left:
-                _traverse(node.left)
-            output = output + str(node.value)
-            if node.right:
-                _traverse(node.right)
+            def _traverse(node):
+                nonlocal output
+                if node.left:
+                    _traverse(node.left)
+                output = output + str(node.value)
+                if node.right:
+                    _traverse(node.right)
+                return output
+            _traverse(self.root)
             return output
-        _traverse(self.root)
-        return output
+        except:
+            return "you have error with insertion by in_order"
 
 
     def post_order(self):
-       
-        output = ''
-        if not self.root:
-            return output
-        def _traverse(node):
-            nonlocal output 
-
-            if node.left:
-                _traverse(node.left)
-            if node.right:
-                _traverse(node.right)
-            output = output + str(node.value)
-            return output
-        _traverse(self.root)
-
-        return output
-
-
-
-
-
-class Binary_Search_Tree:
-
-    def __init__(self) :
-        self.root=None
-
-    def Add(self ,value):
-        if  self.root !=None:
-            self.root = Node(value)
-            
-        else:
+       try:
+            output = ''
+            if not self.root:
+                return output
             def _traverse(node):
-                if value > node.value:
-                    if  node.right != None:
-                        node.right = Node(value)
-                        return
-                    else:
-                        _traverse(node.right)
-                
-                else:
-                    
-                    if  node.left !=None:
-                        node.left = Node(value)
-                        return
-                    else:
-                        _traverse(node.left)
-                        
+                nonlocal output 
+
+                if node.left:
+                    _traverse(node.left)
+                if node.right:
+                    _traverse(node.right)
+                output = output + str(node.value)
+                return output
             _traverse(self.root)
 
-    def Contains(self ,value):
+            return output
+       except:
+            return "you have error with insertion by post_order"
+
+
+
+
+class Binary_Search_Tree(Tree):
+
+    def __init__(self):
+        super().__init__()
         
+
+    def Add(self ,value):
+        try:
+            if  self.root == None :
+                self.root = Node(value)
+                
+            else:
+                def _traverse(node):
+                    if value > node.value :
+                        if  node.right == None:
+                            node.right = Node(value)
+                            return
+                        else:
+                            _traverse(node.right)
+                    
+                    else:
+                        
+                        if  node.left == None:
+                            node.left = Node(value)
+                            return
+                        else:
+                            _traverse(node.left)
+                            
+                _traverse(self.root)
+        except:
+            return "you have error with insertion value "
+
+
+    def Contains(self ,value):
+        try:
+
+            if  self.root==None:
+                return False
+
+            else:
+                node = self.root
+                
+                def _iscontains(node):
+                    if value == node.value:
+                        return True
+
+                    elif value < node.value:
+                        node = node.left
+
+                        if node:
+                            return _iscontains(node)
+
+                    elif value > node.value:
+                        node = node.right
+
+                        if node:
+                            return _iscontains(node)
+                        
+                if _iscontains(node) == True:
+                    return True
+
+                else:
+                    return False
+
+        except:
+            return "you have error value"
+
+
+if __name__=='__main__':
+    tree = Binary_Search_Tree()
+    tree.Add(5)
+    tree.Add(10)
+    tree.Add(1)
+
+    tree.Add(7)
+    tree.Add(4)
+    tree.Add(12)
+    tree.Add(0)
+
+    print(tree.Contains(5))
+    print(tree.Contains(30))
+    print(tree.Contains(12))
+
+
+    print(tree.pre_order())
+    print(tree.in_order())
+    print(tree.post_order())
